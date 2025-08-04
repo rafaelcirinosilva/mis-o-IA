ed469a6f3703319c49f813d00e5938124a91bcf9
rafaelcirinosilva <146239342+rafaelcirinosilva@users.noreply.github.com>
2025-08-04 11:48:30 -0300

    .

---
+++ script.js
@@ -0,0 +1,21 @@
+ const caixaPrincipal = document.querySelector(".caixa-principal");
+
+ const caixaPerguntas = document.querySelector(".caixa-perguntas");
+
+ const caixaAlternativas = document.querySelector(".caixa-alternativas");
+
+ const caixaResultado = document.querySelector(".caixa-resultados");
+
+ const textoResultado= document.querySelector(".texto-resultado");
+
+ const lista = [];
+
+ const perguntas = [
+    {
+        enunciado: "Pergunta 1",
+        alternativas: [
+            "alternativa 1 ",
+            "alternativa 2",
+        ]
+    } //fecha objeto pergunta
+ ]// fecha lista
