bdf8ee65a1f8415a80ab4a739adb3adb9e0ec429
rafaelcirinosilva <146239342+rafaelcirinosilva@users.noreply.github.com>
2025-08-11 08:08:52 -0300

    .

--- script.js
+++ script.js
@@ -25,5 +25,26 @@ const perguntas = [
             "alternativa 2",
         ]
     }
+     {
+        enunciado: "Pergunta 3",
+        alternativas: [
+            "alternativa 1 ",
+            "alternativa 2",
+        ]
+    }
+     {
+        enunciado: "Pergunta 4",
+        alternativas: [
+            "alternativa 1 ",
+            "alternativa 2",
+        ]
+    }
+     {
+        enunciado: "Pergunta 5",
+        alternativas: [
+            "alternativa 1 ",
+            "alternativa 2",
+        ]
+    }
 ]
    
