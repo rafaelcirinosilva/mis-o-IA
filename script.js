35dadc37335f1d995f192f55495d40d9831bbb0d
rafaelcirinosilva <146239342+rafaelcirinosilva@users.noreply.github.com>
2025-08-25 08:23:06 -0300

    .

--- script.js
+++ script.js
@@ -1,15 +1,9 @@
 const caixaPrincipal = document.querySelector('.caixa-principal');
-
 const caixaPerguntas = document.querySelector('.caixa-perguntas');
-
 const caixaAlternativas = document.querySelector('.caixa-alternativas');
-
 const caixaResultado = document.querySelector('.caixa-resultado');
-
 const textoResultado = document.querySelector('.texto-Resultado');
-
 const lista = [];
-
 const perguntas = [
     {
         enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
@@ -50,15 +44,17 @@ const perguntas = [
 
 let atual = 0;
 let perguntaAtual;
+let historiaFinal;
 
  function mostraPergunta(){
     perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent=perguntaAtual.enunciado;
  }
  function mostraAlternativas(){
-    for(const alternativas of perguntaAtual.alternativas){
+    for(const alternativa of perguntaAtual.alternativas){
         const botaoAlternativa=document.createElement("button");
         botaoAlternativa.textContent = alternativas.texto;
+        botao
         caixaAlternativas.appendChild(botaoAlternativa);
     }
  }
